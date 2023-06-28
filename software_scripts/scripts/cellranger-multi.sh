518327c36345de270ab10b6739c8c2f98a5fa9dc
hslyman <hslyman@ucdavis.edu>
2023-06-28 10:52:37 -0700

    updates to SLURM and shell scripts for cellranger vdj and multi

--- software_scripts/scripts/cellranger-multi.sh
+++ software_scripts/scripts/cellranger-multi.sh
@@ -5,22 +5,23 @@ export PATH=/share/workshop/vdj_workshop/Software/cellranger-7.1.0/bin:$PATH
 
 ## Set the parameters for the run
 basedir="/share/workshop/vdj_workshop/${USER}"
-reference="/share/workshop/vdj_workshop/refdata-cellranger-vdj-GRCh38-alts-ensembl-7.1.0"
+reference="/share/workshop/vdj_workshop/Software/refdata-gex-GRCh38-2020-A"
 fastqs="${basedir}/00-RawData"
 outdir="${basedir}/01-Cellranger"
+configs="${fastqs}/multiconfig"
+
 mkdir -p $outdir
 cd $outdir
 
 ## loop over samples in sample sheet, running cellranger on each
 ## NOTE: this script expects a sample sheet with one line per sample to be present in the basedir
+## NOTE: this script expects a config file for each sample to be located in fastqs/multiconfig
 for sample in `cat ${basedir}/samples.txt`
 do
   ## Create the call
-  call="cellranger vdj \
+  call="cellranger multi \
     --id=${sample} \
-    --sample=${sample} \
-    --fastqs=${fastqs} \
-    --reference=${reference}"
+    --csv=${configs}/${sample}_multi.csv"
 
 ## Echo the call
   echo $call
--- software_scripts/scripts_links.md
+++ software_scripts/scripts_links.md
@@ -1,3 +1,11 @@
 ## Links to Scripts
 
-[Cellranger counts](scripts/cellranger-counts.sh)
+These scripts are also located in the workshop directory, under `/share/workshop/vdj_workshop/Software/scripts`. All of the scripts make a few assumptions about the way that the working directory is organized. These assumptions are noted in the script. Please take a moment to ensure that you have all of the required input files and change paths as necessary before running.
+
+### SLURM scripts
+[cellranger-vdj.slurm](scripts/cellranger-vdj.slurm)
+[cellranger-multi.slurm](scripts/cellranger-multi.slurm)
+
+### Shell scripts
+[cellranger-vdj.sh](scripts/cellranger-vdj.sh)
+[cellranger-multi.sh](scripts/cellranger-multi.sh)
